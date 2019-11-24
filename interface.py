e425b7c351e58d50ffa80a189310b704f1e233c9
KuanHsun Lu <will1994@uw.edu>
2019-11-23 17:02:24 -0800

    merge all

--- interface.py
+++ interface.py
@@ -2,42 +2,89 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-
+from visualization_functions import *
 import pandas as pd
 import plotly.graph_objs as go
-
 
 items = pd.read_csv("Data/items.csv")
 reviews = pd.read_csv("Data/reviews.csv")
 review_item = reviews.join(items.set_index('asin'), on='asin', how='inner', lsuffix='_review', rsuffix='_item')
-
-print(review_item.columns)
+helpful_vote = review_item.sort_values('helpfulVotes', ascending=False).drop_duplicates(['asin'])
 brands = sorted(list(set(review_item["brand"])))
 len_brand = len(brands)
 
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+def create_df(helpful_vote):
+    list = {}
+    for i in brands:
+        list[i] = helpful_vote[helpful_vote['brand'] == i]
+    return list
+df = create_df(helpful_vote)
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+fnameDict = {
+    'ASUS': df["ASUS"]["asin"],
+    'Apple': df["Apple"]["asin"],
+    'Google': df["Google"]["asin"],
+    'HUAWEI': df["HUAWEI"]["asin"],
+    'Motorola': df['Motorola']["asin"],
+    'Nokia': df['Nokia']["asin"],
+    'OnePlus': df['OnePlus']["asin"],
+    'Samsung': df['Samsung']["asin"],
+    'Sony': df['Sony']["asin"],
+    'Xiaomi': df['Xiaomi']["asin"]
+}
+
+names = list(fnameDict.keys())
+
+app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H1(id="project_title", style={"textAlign": "center"},
-            children="Visualization of cell phone reviews data"),
-    html.Br(),
-    dcc.Dropdown(
-        id='brand_dropdown',
-        options=[{'label': name, 'value': name} for name in brands],
-        value='ASUS'
-    ),
-    dcc.Graph(id="brand_rating")
-    # dcc.Slider(
-    #     id="brand_slider",
-    #     min=0,
-    #     max=len_brand+1,
-    #     step=None,
-    #     marks=dict([(i+1,name) for i, name in enumerate(brands)]),
-    #     value=1
-    # )
+    html.Div(
+        [
+            html.H1(id="project_title", style={"textAlign": "center"},
+                    children="Visualization of cell phone reviews data"),
+            html.Br(),
+            dcc.Graph(id="sales_volume", figure=brand_counts(review_item)),
+            html.Br(),
+            dcc.Graph(id="overall_rating", figure=plot_stacked_rating_hist_allbrands(review_item)),
+            html.Br(),
+            html.Div([
+                dcc.Dropdown(
+                    id='brand_dropdown',
+                    options=[{'label': name, 'value': name} for name in brands],
+                    style={'height': '30px', 'width': '100px'},
+                    value='ASUS',
+                    clearable=False,
+            ),
+            ],style={'width': '20%', 'display': 'inline-block'}),
 
+            dcc.Graph(id="brand_rating")
+        ]
+    ),
+    html.H2(id="Vote_title",
+            children="Highest vote reviews  selecting cell phone",
+            style={"textAlign": "center"}),
+    html.Div(
+    [
+        html.Div([
+        dcc.Dropdown(
+            id='name-dropdown',
+            options=[{'label':name, 'value':name} for name in names],
+            placeholder="Select a brand",
+            value = list(fnameDict.keys())[0]
+            ),
+            ],style={'width': '20%', 'display': 'inline-block'}),
+
+        html.Div([
+        dcc.Dropdown(
+            id='item-dropdown',
+            placeholder="Select a type",
+            ),
+            ],style={'width': '20%', 'display': 'inline-block'}
+        ),
+        html.Hr(),
+        html.Div(id='display-selected-values')
+    ]
+    ),
 ])
 
 @app.callback(
@@ -46,18 +93,34 @@ app.layout = html.Div([
 def update_brand_rating(brand_name):
     filtered_df = review_item[review_item.brand == brand_name]
     print(filtered_df.head())
-    # print(filtered_df.head())
     ratings = filtered_df.groupby('rating_review').size().reset_index(name='counts')
-    ratings['counts'] = ratings['counts'] / len(filtered_df)
-    print(ratings)
-    data = go.Data([go.Bar(x=list(ratings['rating_review']), y=list(ratings['counts']))])
-    layout = go.Layout(title={"text":"ratings distribution of " + brand_name, "xanchor": "left"}, font={"size":20})
+    ratings['counts'] = ratings['counts']
+
+    data = go.Data([go.Pie(labels=list(ratings['rating_review']), values=list(ratings['counts']))])
+    layout = go.Layout(title={"text":"Pie chart of "+brand_name+"'s ratings",
+                              'x':0.5, 'y':0.9, 'xanchor':'center', 'yanchor':'top'},
+                       font={"size":16})
     figure = go.Figure(data=data, layout=layout)
 
+    colors = ['gold', 'mediumturquoise', 'darkorange', 'lightgreen', 'lightblue']
+    figure.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=16,
+                      marker=dict(colors=colors, line=dict(color='#000000', width=2)))
     return figure
 
+@app.callback(
+    dash.dependencies.Output('item-dropdown', 'options'),
+    [dash.dependencies.Input('name-dropdown', 'value')]
+)
+def update_date_dropdown(name):
+    return [{'label': i, 'value': i} for i in fnameDict[name]]
 
-
+@app.callback(
+    dash.dependencies.Output('display-selected-values', 'children'),
+    [dash.dependencies.Input('item-dropdown', 'value')])
+def set_display_children(selected_value):
+    res = ""
+    res += helpful_vote.loc[helpful_vote['asin'] == selected_value]['body']
+    return res
 
 
 if __name__ == '__main__':
--- visualization_functions.py
+++ visualization_functions.py
@@ -1,3 +1,35 @@
 import pandas as pd
+import plotly.graph_objs as go
 
+def brand_counts(review_item):
+    count = review_item.groupby("brand").size()
+    labels = count.index
+    values = count.values
+    data = go.Data([go.Pie(labels = labels, values = values)])
+    layout = go.Layout(title={"text":"Sales Volume", 'x':0.5, 'y':0.9, 'xanchor':'center', 'yanchor':'top'}, font={"size":20})
+    figure = go.Figure(data=data, layout=layout)
 
+    return figure
+
+def plot_stacked_rating_hist_allbrands(review_item):
+    brands = sorted(list(set(review_item["brand"])))
+    scores = sorted(list(set(review_item["rating_review"])))
+
+    freq_dict = dict()
+    for brand_name in brands:
+        filtered_df = review_item[review_item.brand == brand_name]
+        ratings = filtered_df.groupby('rating_review').size().reset_index(name='counts')
+        ratings['counts'] = ratings['counts'] / len(filtered_df)
+        freq_dict[brand_name] = list(ratings['counts'])
+
+    traces = []
+    for i, s in enumerate(scores):
+        trace = go.Bar(x=brands, y=[freq_dict[brand][i] for brand in brands], name=str(s))
+        traces.append(trace)
+
+    layout = go.Layout(title={'text': "Frequency histogram of all brands' ratings ",
+                              'x':0.5, 'y':0.9, 'xanchor':'center', 'yanchor':'top'},
+                       font={"size": 16}, barmode="stack")
+    figure = go.Figure(data=traces, layout=layout)
+
+    return figure
